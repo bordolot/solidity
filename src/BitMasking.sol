3f2b6c6848bd484c73774a5f041edf8a98c5127b
bordolot <otori29@wp.pl>
2024-04-10 11:39:06 +0200

    update

--- src/BitMasking.sol
+++ src/BitMasking.sol
@@ -17,3 +17,46 @@ contract EVMStoragePackedSlotBytes {
         }
     }
 }
+
+contract BitMasking {
+    /// @notice Creates mask for bytes32 value that consists of 16 bits of 1 on the left side and the rest are zeros
+    /// @dev no comment
+    /// @return mask1 between mask
+    /// @return mask2 final mask
+    // |        256 bits         |
+    // 000 ... 000 | 111 ... 111 |
+    //             | 16 bits of 1
+    function example_mask() public pure returns (bytes32 mask1, bytes32 mask2) {
+        assembly {
+            // 000 ... 001 | 000 ... 000 -> mask1
+            // 000 ... 000 | 111 ... 111 -> mask2
+            // 0x000000000000000000000000000000000000000000000000000000000000ffff -> mask2
+            mask1 := shl(16, 1)
+            mask2 := sub(shl(16, 1), 1)
+        }
+    }
+
+    /// @notice Creates mask for bytes32 value that consist of 16bits of 1 on the specific place in this value
+    /// @dev no comment
+    /// @return mask final mask
+    // 000 ... 000 | 111  ... 111 | 000 ... 000
+    //             | 16 bits of 1 | 32 bits of 0
+    function example_shift_mask() public pure returns (bytes32 mask) {
+        assembly {
+            // 0x0000000000000000000000000000000000000000000000000000ffff00000000 -> mask
+            mask := shl(32, sub(shl(16, 1), 1))
+        }
+    }
+
+    /// @notice Creates NOT mask for bytes32 value that consist of 16bits of 1 on the specific place in this value
+    /// @dev no comment
+    /// @return mask final mask
+    // 111 ... 111 | 000  ... 000 | 111 ... 111
+    //             | 16 bits of 0 | 32 bits of 1
+    function example_shift_not_mask() public pure returns (bytes32 mask) {
+        assembly {
+            // 0xffffffffffffffffffffffffffffffffffffffffffffffffffff0000ffffffff -> mask
+            mask := not(shl(32, sub(shl(16, 1), 1)))
+        }
+    }
+}
--- test/BitMasking.t.sol
+++ test/BitMasking.t.sol
@@ -1,22 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
-import {EVMStoragePackedSlotBytes} from "../src/BitMasking.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import {EVMStoragePackedSlotBytes, BitMasking} from "../src/BitMasking.sol";
 
-contract CounterTest is Test {
-    EVMStoragePackedSlotBytes public bitMasking;
+contract BitMaskingTest is Test {
+    EVMStoragePackedSlotBytes public slotBytes;
+    BitMasking public bitMasking;
 
     function setUp() public {
-        bitMasking = new EVMStoragePackedSlotBytes();
+        slotBytes = new EVMStoragePackedSlotBytes();
+        bitMasking = new BitMasking();
     }
 
-    function test_First() public {
-        bytes32 result = bitMasking.get();
+    function test_First() public view {
+        bytes32 result = slotBytes.get();
         bytes32 itShouldBe = bytes32(
             0x0000000000000000000000000000000000000000000000000000cdcdabababab
         );
         assertEq(result, itShouldBe);
+    }
+
+    function test_Second() public view {
+        (bytes32 value1, bytes32 value2) = bitMasking.example_mask();
+        bytes32 value1ShouldBe = bytes32(
+            0x0000000000000000000000000000000000000000000000000000000000010000
+        );
+        bytes32 value2ShouldBe = bytes32(
+            0x000000000000000000000000000000000000000000000000000000000000ffff
+        );
+        // console2.logBytes32(value1);
+        // console2.logBytes32(value2);
+        assertEq(value1, value1ShouldBe);
+        assertEq(value2, value2ShouldBe);
+    }
+
+    function test_example_shift_mask() public view {
+        bytes32 value = bitMasking.example_shift_mask();
+        bytes32 valueShouldBe = bytes32(
+            0x0000000000000000000000000000000000000000000000000000ffff00000000
+        );
+        assertEq(value, valueShouldBe);
+    }
+
+    function test_example_shift_not_mask() public view {
+        bytes32 value = bitMasking.example_shift_not_mask();
+        bytes32 valueShouldBe = bytes32(
+            0xffffffffffffffffffffffffffffffffffffffffffffffffffff0000ffffffff
+        );
+        assertEq(value, valueShouldBe);
     }
 
     // function testFuzz_SetNumber(uint256 x) public {
